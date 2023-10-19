8f511b6189d8556d71b5d2f7d369dca669d4a787
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2023-10-19 22:53:09 +0700

    a

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use leptos::*;
 
 #[component]
-fn ThemeEngine(children: Children, touch: bool) -> impl IntoView {
+fn ThemeEngine(children: Children, touch: ReadSignal<bool>) -> impl IntoView {
     view! {
-        <div class=move || match touch {
-            true=>"",
-            false=>""
+        <div class=move || match touch.get() {
+            true=>"test1",
+            false=>"test2"
         } style="font-size:3rem; --test:pink;">
             {children()}
         </div>
@@ -14,9 +14,12 @@ fn ThemeEngine(children: Children, touch: bool) -> impl IntoView {
 
 #[component]
 fn App() -> impl IntoView {
+    let (touch, set_touch) = create_signal(true);
+
     view! {
-        <ThemeEngine touch={false}>
+        <ThemeEngine touch={touch}>
         <p style="color:var(--test)">"Hello, world!"</p>
+        <button on:click=move |_| {set_touch.set(!touch.get())}>"Toggle Theme "{move || touch.get()}</button>
     </ThemeEngine>
     }
 }
